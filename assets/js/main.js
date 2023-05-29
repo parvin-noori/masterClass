dc1426b373da00acdba654b4abeffd233ab4a4cc
parvin-noori <parvinnoori0@gmail.com>
2023-05-29 15:50:19 +0330

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+
+    new Swiper(".slider", {
+        loop:true,
+        navigation: {
+            nextEl: ".slider-button-next",
+            prevEl: ".slider-button-prev",
+        },
+    });
+})

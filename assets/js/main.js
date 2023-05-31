ae7e428565c25a6bc266673728151b02348d6f35
parvin-noori <parvinnoori0@gmail.com>
2023-05-31 15:28:50 +0330

    insert trending swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
 $(document).ready(function () {
 
-
+// slider
     new Swiper(".slider", {
         loop:true,
         navigation: {
             nextEl: ".slider-button-next",
             prevEl: ".slider-button-prev",
+        },
+    });
+
+
+
+    // trending swiper
+    new Swiper(".trendingSwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 4,
+            },
+            992:{
+                slidesPerView:4,
+            },
+            1024: {
+                slidesPerView: 5,
+            },
         },
     });
 })

4ea2f84c177d24c30cd57aaa844e2879b66768ba
parvin-noori <parvinnoori0@gmail.com>
2023-06-03 11:09:11 +0330

    insert cookswiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,50 @@ $(document).ready(function () {
 
 // slider
     new Swiper(".slider", {
-        loop:true,
+        loop: true,
         navigation: {
             nextEl: ".slider-button-next",
             prevEl: ".slider-button-prev",
         },
     });
 
+    // cook swiper
+    new Swiper(".cookSwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".cookSwiper-button-next",
+            prevEl: ".cookSwiper-button-prev ",
+        },
+        pagination: {
+            el: ".cookSwiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1.2,
+                // slidesPerGroup: 1.2,
+            },
+            640: {
+                slidesPerView: 2.2,
+                // slidesPerGroup: 2.2,
+            },
+            768: {
+                slidesPerView: 3.2,
+                // slidesPerGroup: 3.2,
+            },
+            992: {
+                slidesPerView: 4.2,
+                // slidesPerGroup: 4.2,
+                // allowTouchMove: false,
+            },
+            1024: {
+                slidesPerView: 4.2,
+                // slidesPerGroup: 4.2,
+                // allowTouchMove: false,
+            },
+        },
+    });
 
 
     // trending swiper
@@ -24,20 +61,27 @@ $(document).ready(function () {
             clickable: true,
         },
         breakpoints: {
-            0:{
+            0: {
                 slidesPerView: 1.2,
+                // slidesPerGroup: 1.2,
             },
             640: {
                 slidesPerView: 2.2,
+                // slidesPerGroup: 2.2,
             },
             768: {
                 slidesPerView: 3.2,
+                // slidesPerGroup: 3.2,
             },
-            992:{
-                slidesPerView:4.2,
+            992: {
+                slidesPerView: 4.2,
+                // slidesPerGroup: 4.2,
+                // allowTouchMove: false,
             },
             1024: {
                 slidesPerView: 4.2,
+                // slidesPerGroup: 4.2,
+                // allowTouchMove: false,
             },
         },
     });

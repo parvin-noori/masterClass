2b89490d03688cd2135eaa542948ac4255c3ec80
parvin-noori <parvinnoori0@gmail.com>
2023-06-14 15:50:01 +0330

    insert scrollspy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,246 +1,274 @@
 $(document).ready(function () {
 
 // slider
-    new Swiper(".slider", {
-        loop: true,
-        navigation: {
-            nextEl: ".slider-button-next",
-            prevEl: ".slider-button-prev",
-        },
-    });
-
-    // cook swiper
-    new Swiper(".cookSwiper", {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: ".cookSwiper-button-next",
-            prevEl: ".cookSwiper-button-prev ",
-        },
-        pagination: {
-            el: ".cookSwiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1.2,
-                // slidesPerGroup: 1.2,
+        new Swiper(".slider", {
+            loop: true,
+            navigation: {
+                nextEl: ".slider-button-next",
+                prevEl: ".slider-button-prev",
             },
-            576: {
-                slidesPerView: 2.2,
-                // slidesPerGroup: 2.2,
+        });
+
+        // cook swiper
+        new Swiper(".cookSwiper", {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            navigation: {
+                nextEl: ".cookSwiper-button-next",
+                prevEl: ".cookSwiper-button-prev ",
             },
-            768: {
-                slidesPerView: 3.2,
-                // slidesPerGroup: 3.2,
+            pagination: {
+                el: ".cookSwiper-pagination",
+                clickable: true,
             },
-            992: {
-                slidesPerView: 4.2,
-                // slidesPerGroup: 4.2,
-                // allowTouchMove: false,
+            breakpoints: {
+                0: {
+                    slidesPerView: 1.2,
+                    // slidesPerGroup: 1.2,
+                },
+                576: {
+                    slidesPerView: 2.2,
+                    // slidesPerGroup: 2.2,
+                },
+                768: {
+                    slidesPerView: 3.2,
+                    // slidesPerGroup: 3.2,
+                },
+                992: {
+                    slidesPerView: 4.2,
+                    // slidesPerGroup: 4.2,
+                    // allowTouchMove: false,
+                },
+                1024: {
+                    slidesPerView: 4.2,
+                    // slidesPerGroup: 4.2,
+                    // allowTouchMove: false,
+                },
             },
-            1024: {
-                slidesPerView: 4.2,
-                // slidesPerGroup: 4.2,
-                // allowTouchMove: false,
+        });
+
+
+        // trending swiper
+        new Swiper(".trendingSwiper", {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            navigation: {
+                nextEl: ".trendingSwiper-button-next",
+                prevEl: ".trendingSwiper-button-prev",
             },
-        },
-    });
-
-
-    // trending swiper
-    new Swiper(".trendingSwiper", {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: ".trendingSwiper-button-next",
-            prevEl: ".trendingSwiper-button-prev",
-        },
-        pagination: {
-            el: ".trendingSwiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1.2,
-                // slidesPerGroup: 1.2,
+            pagination: {
+                el: ".trendingSwiper-pagination",
+                clickable: true,
             },
-            576: {
-                slidesPerView: 2.2,
-                // slidesPerGroup: 2.2,
+            breakpoints: {
+                0: {
+                    slidesPerView: 1.2,
+                    // slidesPerGroup: 1.2,
+                },
+                576: {
+                    slidesPerView: 2.2,
+                    // slidesPerGroup: 2.2,
+                },
+                768: {
+                    slidesPerView: 3.2,
+                    // slidesPerGroup: 3.2,
+                },
+                992: {
+                    slidesPerView: 4.2,
+                    // slidesPerGroup: 4.2,
+                    // allowTouchMove: false,
+                },
+                1024: {
+                    slidesPerView: 4.2,
+                    // slidesPerGroup: 4.2,
+                    // allowTouchMove: false,
+                },
             },
-            768: {
-                slidesPerView: 3.2,
-                // slidesPerGroup: 3.2,
+        });
+
+
+        // reviews swiper
+        new Swiper(".reviewsSwiper", {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            navigation: {
+                nextEl: ".reviewsSwiper-button-next",
+                prevEl: ".reviewsSwiper-button-prev",
             },
-            992: {
-                slidesPerView: 4.2,
-                // slidesPerGroup: 4.2,
-                // allowTouchMove: false,
+            pagination: {
+                el: ".reviewsSwiper-pagination",
+                clickable: true,
             },
-            1024: {
-                slidesPerView: 4.2,
-                // slidesPerGroup: 4.2,
-                // allowTouchMove: false,
+            breakpoints: {
+                0: {
+                    slidesPerView: 1.2,
+                    // slidesPerGroup: 1.2,
+                },
+                576: {
+                    slidesPerView: 1.5,
+                    // slidesPerGroup: 2.2,
+                },
+                768: {
+                    slidesPerView: 1,
+                    // slidesPerGroup: 3.2,
+                },
+                992: {
+                    slidesPerView: 1,
+                    // slidesPerGroup: 4.2,
+                    // allowTouchMove: false,
+                },
+                1024: {
+                    slidesPerView: 1,
+                    // slidesPerGroup: 4.2,
+                    // allowTouchMove: false,
+                },
             },
-        },
-    });
+        });
+
+        //copy link
+        let currentUrl = window.location.href;
+        $('#copyLinkInput').val(currentUrl);
 
 
-    // reviews swiper
-    new Swiper(".reviewsSwiper", {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: ".reviewsSwiper-button-next",
-            prevEl: ".reviewsSwiper-button-prev",
-        },
-        pagination: {
-            el: ".reviewsSwiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1.2,
-                // slidesPerGroup: 1.2,
-            },
-            576: {
-                slidesPerView: 1.5,
-                // slidesPerGroup: 2.2,
-            },
-            768: {
-                slidesPerView: 1,
-                // slidesPerGroup: 3.2,
-            },
-            992: {
-                slidesPerView: 1,
-                // slidesPerGroup: 4.2,
-                // allowTouchMove: false,
-            },
-            1024: {
-                slidesPerView: 1,
-                // slidesPerGroup: 4.2,
-                // allowTouchMove: false,
-            },
-        },
-    });
+        // copyLinkBtn
+        $('#copyLinkBtn').click(function (e) {
+            // Get the text field
+            var copyText = document.getElementById("copyLinkInput");
 
-    //copy link
-    let currentUrl = window.location.href;
-    $('#copyLinkInput').val(currentUrl);
+            // // Select the text field
+            // copyText.select();
+            // copyText.setSelectionRange(0, 99999); // For mobile devices
+
+            // Copy the text inside the text field
+            navigator.clipboard.writeText(copyText.value);
+
+            // Provide visual feedback to the user (optional)
+            $(this).text('Copied!');
+
+            // Reset the button text after a brief delay (optional)
+            setTimeout(function () {
+                $('#copyLinkBtn').text('Copy');
+            }, 2000);
+
+        })
+
+        //responsive copy link
+        $('.responsiveCopyLink').click(function () {
+            navigator.clipboard.writeText(currentUrl);
+        })
+
+        //video js(trailer modal)
+
+        let video = videojs("my-video");
+        const myModalEl = document.getElementById('trailerModal');
+        let overlay;
+        myModalEl.addEventListener('shown.bs.modal', event => {
+            overlay = $(this).find('.overlay');
+            video.ready(function () {
+                // this.play();
+                video.vhs = null;
+
+            });
+            video.on("play", function () {
+                overlay.addClass('d-none');
+                overlay.removeClass('d-flex')
+            });
+            video.on("pause", function () {
+                overlay.addClass('d-flex');
+                overlay.removeClass('d-none')
+            })
+            //
+        })
 
 
-    // copyLinkBtn
-    $('#copyLinkBtn').click(function (e) {
-        // Get the text field
-        var copyText = document.getElementById("copyLinkInput");
+        //video js(about this class)
+        let video2 = videojs("video2");
 
-        // // Select the text field
-        // copyText.select();
-        // copyText.setSelectionRange(0, 99999); // For mobile devices
-
-        // Copy the text inside the text field
-        navigator.clipboard.writeText(copyText.value);
-
-        // Provide visual feedback to the user (optional)
-        $(this).text('Copied!');
-
-        // Reset the button text after a brief delay (optional)
-        setTimeout(function () {
-            $('#copyLinkBtn').text('Copy');
-        }, 2000);
-
-    })
-
-    //responsive copy link
-    $('.responsiveCopyLink').click(function () {
-        navigator.clipboard.writeText(currentUrl);
-    })
-
-    //video js(trailer modal)
-
-    let video = videojs("my-video");
-    const myModalEl = document.getElementById('trailerModal');
-    let overlay;
-    myModalEl.addEventListener('shown.bs.modal', event => {
-        overlay = $(this).find('.overlay');
-        video.ready(function () {
-            // this.play();
-            video.vhs = null;
+        video2.ready(function () {
+            overlay = $('#' + $(this).attr('id')).closest('.video-wrapper').find('.overlay')
+            video2.vhs = null;
 
         });
-        video.on("play", function () {
+        video2.on("play", function () {
             overlay.addClass('d-none');
             overlay.removeClass('d-flex')
         });
-        video.on("pause", function () {
+        video2.on("pause", function () {
             overlay.addClass('d-flex');
             overlay.removeClass('d-none')
         })
-        //
-    })
 
-
-    //video js(about this class)
-    let video2 = videojs("video2");
-
-    video2.ready(function () {
-        overlay = $('#' + $(this).attr('id')).closest('.video-wrapper').find('.overlay')
-        video2.vhs = null;
-
-    });
-    video2.on("play", function () {
-        overlay.addClass('d-none');
-        overlay.removeClass('d-flex')
-    });
-    video2.on("pause", function () {
-        overlay.addClass('d-flex');
-        overlay.removeClass('d-none')
-    })
-
-    //category classes swiper
-    var swiper = new Swiper(".category-classes-swiper", {
-        slidesPerView: 1,
-        spaceBetween: 15,
-        navigation: {
-            nextEl: ".category-classes-swiper-next",
-            prevEl: ".category-classes-swiper-prev",
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1.1,
+        //category classes swiper
+        var swiper = new Swiper(".category-classes-swiper", {
+            slidesPerView: 1,
+            spaceBetween: 15,
+            navigation: {
+                nextEl: ".category-classes-swiper-next",
+                prevEl: ".category-classes-swiper-prev",
             },
-            574: {
-                slidesPerView: 2,
+            breakpoints: {
+                0: {
+                    slidesPerView: 1.1,
+                },
+                574: {
+                    slidesPerView: 2,
+                },
+                640: {
+                    slidesPerView: 3,
+                },
+                768: {
+                    slidesPerView: 4,
+                },
+                992: {
+                    slidesPerView: 5,
+                },
+                1200: {
+                    slidesPerView: 6,
+                },
             },
-            640: {
-                slidesPerView: 3,
-            },
-            768: {
-                slidesPerView: 4,
-            },
-            992: {
-                slidesPerView: 5,
-            },
-            1200: {
-                slidesPerView: 6,
-            },
-        },
-    });
+        });
 
-    //scrollspy
+        //scrollspy
 
-    // Hide Header on scroll down
-    let prevScrollPos = window.pageYOffset;
+        // Hide Header on scroll down
+        let prevScrollPos = window.pageYOffset;
+        let navbarHeight = $('header').outerHeight();
 
-    window.onscroll = function () {
-        let navbarCoursesTab = $('#navbar-courses-tab');
-        let navbarHeight = jQuery('header').outerHeight();
-        let currentScrollPos = window.pageYOffset;
-        if (currentScrollPos > navbarCoursesTab.offset().top) {
-            navbarCoursesTab.addClass('position-sticky');
-            navbarCoursesTab.css({'top': navbarHeight})
-            prevScrollPos = currentScrollPos;
+        let PSHeight = $('#navbar-courses-tab').outerHeight()
+        window.onscroll = function () {
+            let navbarCoursesTab = $('#navbar-courses-tab');
+            let currentScrollPos = window.pageYOffset;
+            if (currentScrollPos > navbarCoursesTab.offset().top) {
+                navbarCoursesTab.addClass('position-sticky');
+                navbarCoursesTab.css({'top': navbarHeight})
+                prevScrollPos = currentScrollPos;
+            }
+        }
+
+
+        //scrollspy
+        // Cache the navigation links
+        var $navigationLinks = jQuery('#navbar-courses-tab ul.nav li a');
+
+
+        let navbarCoursesTab = jQuery('#navbar-courses-tab');
+
+        if (navbarCoursesTab.length > 0) {
+            // Add click handlers to the navigation links
+            $navigationLinks.on('click', function (event) {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+                // Store the hash (#)
+                var hash = this.hash;
+                jQuery('html, body').animate({
+                    scrollTop: jQuery(hash).offset().top - (navbarHeight + PSHeight) // subtract the fixed header height and some padding
+
+                }, 300, function () {
+
+                });
+
+            });
+
         }
     }
-})
+)
